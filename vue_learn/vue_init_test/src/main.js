83a2e40c9c01c7f1316d7887e2aa57069cfd8cb4
yxnne <506524339@qq.com>
2018-06-21 15:33:43 +0800

    vue very easy demo , an todo list

--- vue_learn/vue_init_test/src/main.js
+++ vue_learn/vue_init_test/src/main.js
@@ -6,8 +6,47 @@ import App from './App'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   components: { App },
+//   template: '<App/>'
+// })
+
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
+	el:'#app',
+	// 数据
+	data:{
+		title:'Simple Demo in Vue: Todo List',
+		showSubTitle:true,
+		newtodo:'',
+		subTitle:'A Very Simple Demo',
+		todos:[{text:'Never Play Games',done:false}],
+	},
+	//计算属性
+	computed:{
+		remains:function(){
+			return this.todos.filter(i=>i.done).length;
+		}
+	},
+	// 方法集合
+	methods:{
+		// es6 before function
+		handleAdd:function(){
+			// this.title = "shit"
+			this.todos.push({
+				text:this.newtodo,
+				done:false
+			});
+			this.newtodo = '';
+
+		},
+		// es6 function writing style
+		clearFinish(){
+			this.todos = this.todos.filter(i=> !i.done);
+		},
+		toggle:function(index){
+			console.log(index);
+			this.todos[index].done = !this.todos[index].done;
+		},
+	},
 })
