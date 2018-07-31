213e28e69d8ff14fa6f4ce8078a0deffb7a9fb66
yxnne <506524339@qq.com>
2018-07-31 17:27:17 +0800

    oo

---
+++ js_learn/js_oo.js
@@ -0,0 +1,15 @@
+// js构造对象
+/*
+ 每一个函数都是一个对象（Function），但是Function也是对象Object；
+ 
+ 构造函数就是调用函数，使用new调用函数和不适用new都能调用函数，
+ Function()
+ new Function()
+ 有啥区别呢？？
+ 带上new ，会新建一个对象 ，将this指向新的对象
+	
+ 生成新对象：
+ 1. 可以通过new Object();
+
+
+*/
