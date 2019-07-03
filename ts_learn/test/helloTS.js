a3ed5762f6e68b24fbb3d1c94e72984e04a18a96
ddcat <ddcat@ddcatdeMacBook-Pro.local>
2019-07-03 22:24:07 +0800

    test the first TS

---
+++ ts_learn/test/helloTS.js
@@ -0,0 +1,4 @@
+function sayHello(toWhom) {
+    return 'Hello' + toWhom;
+}
+console.log(sayHello('TYPE SCRIPT'));
---
+++ ts_learn/test/helloTS.ts
@@ -0,0 +1,5 @@
+function sayHello(toWhom){
+    return 'Hello, ' + toWhom;
+}
+
+console.log(sayHello('TYPE SCRIPT'));
